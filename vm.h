ca7a7be1d2c8e675ac6e5d5ad284b7c33567b4e5
Pr15h <priyadarshi_s@ma.iitr.ac.in>
2025-12-02 20:41:27 +0530

    added reg mapping

--- vm.h
+++ vm.h
@@ -7,120 +7,112 @@ extern "C" {
 
 #include <stdint.h>
 #include <stddef.h>
-#include <sys/types.h> 
-#ifndef VM_NUM_REGS
+#include <sys/types.h>
+
+typedef uint8_t  vm_byte_t;
+typedef uint64_t vm_word_t;
+
+#define VM_WORD_BYTES (sizeof(vm_word_t))
+
 #define VM_NUM_REGS 16
-#endif
 
-#ifndef VM_MEM_SIZE
-#define VM_MEM_SIZE (64 * 1024) 
-#endif
+typedef enum {
+    RAX = 0,
+    RBX = 1,
+    RCX = 2,
+    RDX = 3,
+    RSI = 4,
+    RDI = 5,
+    RBP = 6,
+    RSP = 7,
+    R8  = 8,
+    R9  = 9,
+    R10 = 10,
+    R11 = 11,
+    R12 = 12,
+    R13 = 13,
+    R14 = 14,
+    R15 = 15
+} reg_t;
 
-#ifndef VM_CALLSTACK_MAX
-#define VM_CALLSTACK_MAX 1024
-#endif
-
-#ifndef VM_DATASTACK_MAX
-#define VM_DATASTACK_MAX 1024
-#endif
-
-typedef int64_t vm_word_t;
-typedef uint8_t vm_byte_t;
-
-enum {
+typedef enum {
     VM_OK = 0,
-    VM_ERR_ALLOC = 1,
-    VM_ERR_INVALID = 2,
-    VM_ERR_DIV_ZERO = 3,
-    VM_ERR_MEM_OOB = 4,
-    VM_ERR_STACK_UNDERFLOW = 5,
-    VM_ERR_STACK_OVERFLOW = 6,
-    VM_ERR_CALLSTACK_OVERFLOW = 7,
-    VM_ERR_CALLSTACK_UNDERFLOW = 8,
-    VM_ERR_UNKNOWN_OPCODE = 9,
-};
+    VM_ERR_OOB = -1,
+    VM_ERR_STACK_UNDERFLOW = -2,
+    VM_ERR_STACK_OVERFLOW = -3,
+    VM_ERR_CALLSTACK_OVERFLOW = -4,
+    VM_ERR_CALLSTACK_UNDERFLOW = -5,
+    VM_ERR_BAD_OPCODE = -6,
+    VM_ERR_DIV_BY_ZERO = -7,
+    VM_ERR_HALT = 1,  
+    VM_ERR_NO_MEM = -8,
+    VM_ERR_INVAL = -9
+} vm_error_t;
 
-enum VM_Opcode {
-    OP_HLT               = 0x00,
-    OP_NOP               = 0x01,
-    OP_MOV_REG_REG       = 0x02,
-    OP_MOV_REG_IMM       = 0x03,
-    OP_ADD               = 0x04,
-    OP_SUB               = 0x05,
-    OP_MUL               = 0x06,
-    OP_DIV               = 0x07,
-    OP_AND               = 0x08,
-    OP_OR                = 0x09,
-    OP_XOR               = 0x0A,
-    OP_SHL               = 0x0B,
-    OP_SHR               = 0x0C,
-    OP_LOAD_REG_MEM      = 0x0D,
-    OP_STORE_MEM_REG     = 0x0E,
-    OP_JMP               = 0x0F,
-    OP_JZ                = 0x10,
-    OP_JNZ               = 0x11,
-    OP_CMP               = 0x12,
-    OP_CALL              = 0x13,
-    OP_RET               = 0x14,
-    OP_PRINT_REG         = 0x15,
-    OP_PRINT_IMM         = 0x16,
-    OP_PUSH_REG          = 0x17,
-    OP_POP_REG           = 0x18,
-};
+typedef enum {
+    OP_NOP = 0x00,
+    OP_HALT = 0x01,
+
+   
+    OP_MOV_RI = 0x10, 
+    OP_MOV_RR = 0x11,
+    OP_ADD = 0x20,      
+    OP_SUB = 0x21,
+    OP_MUL = 0x22,
+    OP_DIV = 0x23,
+    OP_LOAD = 0x30,
+    OP_STORE = 0x31, 
+    OP_PUSH = 0x40,     
+    OP_POP  = 0x41,      
+    OP_JMP  = 0x50,      
+    OP_JZ   = 0x51,      
+    OP_JNZ  = 0x52,      
+    OP_CALL = 0x53,     
+    OP_RET  = 0x54,      
+    OP_CMP_RR = 0x60,    
+
+    OP_PRINT_R = 0x70, 
+
+    OP_DB = 0xFF
+} opcode_t;
 
 typedef struct VM {
-    vm_word_t regs[VM_NUM_REGS];    
 
-    
+    vm_word_t regs[VM_NUM_REGS];
+
     vm_byte_t *code;
     size_t code_size;
 
-   
     vm_byte_t *mem;
     size_t mem_size;
-    int owns_code; 
-    int owns_mem; 
-  
-    ssize_t callstack[VM_CALLSTACK_MAX];
+
+    int owns_code;
+    int owns_mem;
+
+
+    ssize_t callstack[1024];
     int callsp;
 
-   
-    vm_word_t datstack[VM_DATASTACK_MAX];
-    int datsp;
-
     ssize_t pc;
-
 
     int ZF;
     int SF;
 
-   
-    int debug; 
+    int debug;
 
     int last_error;
 
 } VM;
 
-
-
-
 int vm_init(VM *vm, const vm_byte_t *code, size_t code_size, int want_copy);
-
 void vm_free(VM *vm);
-
 int vm_run(VM *vm);
-
 int vm_step(VM *vm);
-
 int vm_load_program(VM *vm, const vm_byte_t *code, size_t code_size);
-
 void vm_set_debug(VM *vm, int debug_on);
-
 int vm_read_mem(VM *vm, size_t addr, vm_word_t *out);
 int vm_write_mem(VM *vm, size_t addr, vm_word_t val);
-
 int vm_last_error(VM *vm);
-
 int vm_disasm_at(VM *vm, ssize_t pc, char *outbuf, size_t buflen);
 
 #ifdef __cplusplus
@@ -128,5 +120,3 @@ int vm_disasm_at(VM *vm, ssize_t pc, char *outbuf, size_t buflen);
 #endif
 
 #endif
-
-
