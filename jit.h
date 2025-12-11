18a3a09b1b270c6874f0cc870e805cd17803c63a
Pr15h <priyadarshi_s@ma.iitr.ac.in>
2025-12-11 21:05:07 +0530

    Final changes

--- jit.h
+++ jit.h
@@ -1,19 +1,31 @@
 #ifndef JIT_H
 #define JIT_H
 
-#include <stdint.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <stddef.h>
 #include "vm.h"
 
-
-typedef struct {
-    void *code;      
-    size_t size;    
-    int (*fn)(VM *vm); 
+typedef struct JITUnit {
+    void *code;
+    size_t size;
+    int  (*fn)(VM *vm);
 } JITUnit;
 
 JITUnit *jit_compile(VM *vm, const vm_byte_t *code, size_t code_size);
 
 void jit_free(JITUnit *u);
+
+#define JIT_ERR_STALE_MAP 1
+
+#ifdef HAS_JIT_RUN_UNIT
+int jit_run_unit(JITUnit *unit, VM *vm);
+#endif
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
