2000fc552297c28e01de9df9b8fe3ac78b447743
Pr15h <priyadarshi_s@ma.iitr.ac.in>
2025-12-11 21:04:13 +0530

    Update vm.h

--- vm.h
+++ vm.h
@@ -9,13 +9,18 @@ extern "C" {
 #include <stddef.h>
 #include <sys/types.h>
 
+#include "magic_expand.h"
+
 typedef uint8_t  vm_byte_t;
 typedef uint64_t vm_word_t;
-
+#define OP_MAGIC 0xF0
+#define OP_MAGIC_SITE_HOLES 0x42
 #define VM_WORD_BYTES (sizeof(vm_word_t))
 
-#define VM_NUM_REGS 16
-
+#define VM_NUM_REGS 128
+#ifndef REG_RAX
+#define REG_RAX 0
+#endif
 typedef enum {
     RAX = 0,
     RBX = 1,
@@ -44,7 +49,7 @@ typedef enum {
     VM_ERR_CALLSTACK_UNDERFLOW = -5,
     VM_ERR_BAD_OPCODE = -6,
     VM_ERR_DIV_BY_ZERO = -7,
-    VM_ERR_HALT = 1,  
+    VM_ERR_HALT = 1,
     VM_ERR_NO_MEM = -8,
     VM_ERR_INVAL = -9
 } vm_error_t;
@@ -53,31 +58,29 @@ typedef enum {
     OP_NOP = 0x00,
     OP_HALT = 0x01,
 
-   
-    OP_MOV_RI = 0x10, 
+    OP_MOV_RI = 0x10,
     OP_MOV_RR = 0x11,
-    OP_ADD = 0x20,      
+    OP_ADD = 0x20,
     OP_SUB = 0x21,
     OP_MUL = 0x22,
     OP_DIV = 0x23,
     OP_LOAD = 0x30,
-    OP_STORE = 0x31, 
-    OP_PUSH = 0x40,     
-    OP_POP  = 0x41,      
-    OP_JMP  = 0x50,      
-    OP_JZ   = 0x51,      
-    OP_JNZ  = 0x52,      
-    OP_CALL = 0x53,     
-    OP_RET  = 0x54,      
-    OP_CMP_RR = 0x60,    
+    OP_STORE = 0x31,
+    OP_PUSH = 0x40,
+    OP_POP  = 0x41,
+    OP_JMP  = 0x50,
+    OP_JZ   = 0x51,
+    OP_JNZ  = 0x52,
+    OP_CALL = 0x53,
+    OP_RET  = 0x54,
+    OP_CMP_RR = 0x60,
 
-    OP_PRINT_R = 0x70, 
+    OP_PRINT_R = 0x70,
 
     OP_DB = 0xFF
 } opcode_t;
 
 typedef struct VM {
-
     vm_word_t regs[VM_NUM_REGS];
 
     vm_byte_t *code;
@@ -88,7 +91,6 @@ typedef struct VM {
 
     int owns_code;
     int owns_mem;
-
 
     ssize_t callstack[1024];
     int callsp;
@@ -102,6 +104,7 @@ typedef struct VM {
 
     int last_error;
 
+    int holes[40];
 } VM;
 
 int vm_init(VM *vm, const vm_byte_t *code, size_t code_size, int want_copy);
